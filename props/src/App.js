fb89849b5da04d8236a90b383369129d7bd2c5a9
Sebastian Tleye <stleye@gmail.com>
2024-02-08 13:46:19 -0300

    Added props

---
+++ props/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Heading from "./Heading";
+
+function App() {
+  return (
+    <div className="App">
+        <Heading firstName="Bob"/>
+        <Heading firstName="Rose"/>
+    </div>
+  );
+}
+
+export default App;
