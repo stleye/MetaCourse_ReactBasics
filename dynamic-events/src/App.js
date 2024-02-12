c19afa5ff0db8e86f62c68ef5a4926feda7f4678
Sebastian Tleye <stleye@gmail.com>
2024-02-12 09:09:10 -0300

    Added dynamic-events

---
+++ dynamic-events/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+
+  function handleClick() { 
+    let randomNum = Math.floor(Math.random() * 3) + 1;
+    console.log(randomNum);
+    let userInput = prompt('type a number'); 
+    alert(`Computer number: ${randomNum}, Your guess: ${userInput}`);
+  }
+
+  return (
+    <div>
+      <button tag='button' onClick={handleClick}>Guess the number between 1 and 3</button>
+      <h1>Task: Add a button and handle a click event</h1>
+    </div>
+  );
+}
+
+export default App;
